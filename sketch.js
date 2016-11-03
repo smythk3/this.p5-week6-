e9d965f39913339f2d7bed034d2c6c9883bb91b9
smythk3 <smythk3@tcd.ie>
2016-11-03 15:39:37 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var circle = {
+ // name : value
+ 
+ x: 100,
+ y: 100,
+ display : function {
+   ellipse(this.x, this.y, 50, 50);
+   // use "this" to refer to the particular parameters of a particular circle
+ }
+ 
+};
+
+function setup() {
+
+}
+
+function draw() {
+  
+}
